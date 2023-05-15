8f17edbb849cd07965fd172878b95f3296a6db66
pedrocpacheco <cpedropacheco@hotmail.com>
2023-05-15 09:41:19 -0300

    feat: erro function, multiple validations

--- 1-Semestre/Aulas/Mes 5/15/script.js
+++ 1-Semestre/Aulas/Mes 5/15/script.js
@@ -4,13 +4,39 @@ function validar(){
     var nome = form.nome // Nome é o nome de um dos campos, e ai pegamos o seu value
     var senha =  form.senha // Mesma coisa com a senha
 
+    
     if(nome.value == "" || nome.value == " "){
-        alert("Escreve algo no nome ai seu merdinha")
-        
-        nome.style.backgroundColor = "red"
-        nome.focus() // Faz o cursor "ir" pro campo automaticamente
+        erro(nome, "Escreve algo no nome ai")
+        return false
+    }
 
-        document.getElementById("erro").innerText = "ERRO NA SEÇÃO NOME"
-        return false // Quando validamos algo, e vimos que da ruim, precisamos retornar false // Para a ação do formulario parar
+    else if(senha.value == "1234" || senha.value == "" || senha.value == " "){
+        erro(senha, "Tu ta de sacanagem que vai meter essa senha?") 
+        return false
     }
 }
+
+function erro(secao, mensagem){
+    alert(mensagem)
+    secao.style.backgroundColor = "red"
+    secao.focus()
+
+    document.getElementById("erro").innerText = "ERRO NA SEÇÃO " + secao.name.toUpperCase()
+}
+
+
+
+
+
+
+
+
+    // if(nome.value == "" || nome.value == " "){
+    //     alert("Escreve algo no nome ai seu merdinha")
+
+    //     nome.style.backgroundColor = "red"
+    //     nome.focus() // Faz o cursor "ir" pro campo automaticamente
+
+    //     document.getElementById("erro").innerText = "ERRO NA SEÇÃO NOME"
+    //     return false // Quando validamos algo, e vimos que da ruim, precisamos retornar false // Para a ação do formulario parar
+    // }
