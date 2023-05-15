c7411a74210f1b7249805f8aac4920c780884a6b
pedrocpacheco <cpedropacheco@hotmail.com>
2023-05-15 09:02:53 -0300

    feat: changing nome input's info after a erronic submit

--- 1-Semestre/Aulas/Mes 5/15/script.js
+++ 1-Semestre/Aulas/Mes 5/15/script.js
@@ -1,15 +1,16 @@
 function validar(){
-    
     // Salvando valores de campos de formulario em variaveis
-    var caminho = document.frLogin // Definindo uma variavel caminho pra nao ter que escrever sempre
-    let nome = caminho.nome.value // Nome é o nome de um dos campos, e ai pegamos o seu value
-    var senha =  caminho.senha.value // Mesma coisa com a senha
+    var form = document.frLogin // Definindo uma variavel form pra nao ter que escrever sempre
+    var nome = form.nome // Nome é o nome de um dos campos, e ai pegamos o seu value
+    var senha =  form.senha // Mesma coisa com a senha
 
-    if(nome == "" || nome == " "){
+    if(nome.value == "" || nome.value == " "){
         alert("Escreve algo no nome ai seu merdinha")
-        return false // Quando validamos algo, e vimos que da ruim, precisamos retornar false
-                     // Para a ação do formulario parar
-    }
+        
+        nome.style.backgroundColor = "red"
+        nome.focus() // Faz o cursor "ir" pro campo automaticamente
 
-    document.getElementById("erro").innerText = nome + " " + senha
+        document.getElementById("erro").innerText = "ERRO NA SEÇÃO NOME"
+        return false // Quando validamos algo, e vimos que da ruim, precisamos retornar false // Para a ação do formulario parar
+    }
 }
