a8759824d5f7b6d3806e6f7598e931247693eeca
Logon Aluno <logonrmlocal@fiap.com.br>
2023-05-22 09:09:40 -0300

    feat: getting a especific character from a string

--- 1-Semestre/Aulas/Mes 5/22 - Random Password/script.js
+++ 1-Semestre/Aulas/Mes 5/22 - Random Password/script.js
@@ -10,3 +10,28 @@ const upperLetters = abecedario.toUpperCase()
 const lowerLetters = abecedario.toLowerCase()
 const numbers = "0123456789"
 const symbols = "!@#$%&*()^~+-|_"
+
+function getIndex(string){
+    index = Math.floor(Math.random() * string.length)
+    returnindex
+}
+
+function getUpperCase(){
+    number = getIndex(upperLetters)
+    return upperLetters[number]
+}
+
+function getLowerCase(){
+    number = getIndex(lowerLetters)
+    return lowerLetters[number]
+}
+
+function getNumber(){
+    number = getIndex(numbers)
+    return numbers[number]
+}
+
+function getSymbol(){
+    number = getIndex(symbols)
+    return symbols[number]
+}
