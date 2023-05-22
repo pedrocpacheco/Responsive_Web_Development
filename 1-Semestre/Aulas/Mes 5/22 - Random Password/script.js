411a5f41530f214c2c28ab8aac38cd9750885b61
Logon Aluno <logonrmlocal@fiap.com.br>
2023-05-22 08:48:25 -0300

    feat: saving the elements by it's id and creating more variables

---
+++ 1-Semestre/Aulas/Mes 5/22 - Random Password/script.js
@@ -0,0 +1,12 @@
+const upperEL = document.getElementById("upper")
+const lowerEL = document.getElementById("lower")
+const numberEL = document.getElementById("number")
+const symbolsEL = document.getElementById("symbols")
+const generateEL = document.getElementById("generate")
+const pwEL = document.getElementById("pw")
+
+const abecedario = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+const upperLetters = abecedario.toUpperCase()
+const lowerLetters = abecedario.toLowerCase()
+const numbers = "0123456789"
+const symbols = "!@#$%&*()^~+-|_"
