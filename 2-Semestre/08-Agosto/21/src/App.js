ca4816d8945fd3488e3dc33a348a046238c31287
Logon Aluno <logonrmlocal@fiap.com.br>
2023-08-21 09:41:19 -0300

    exercise

--- 2-Semestre/08-Agosto/21/src/App.js
+++ 2-Semestre/08-Agosto/21/src/App.js
@@ -1,7 +1,17 @@
 import React from "react";
+import "./App.css" // ? Importar CSS vai direto, sem alias
+import Componente from "./Componente";
 
 export default function App(){
   return <>
-    Corinthians
+    <h1 className="title">Corinthians</h1>
+    <h1 style={ styles.paragraph }>Salve o Corinthians o Campeão dos Campeões</h1>
+    <Componente />
   </>
+}
+
+const styles = {
+  paragraph : {
+    fontSize: '2em'
+  }
 }
